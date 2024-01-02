5779791a90ecfd2144ed882aee83bbb20b48d6e4
DarshitRajyaguru <darshitrajyaguru@gmail.com>
2024-01-02 16:12:01 +0530

    nav added

--- functions.php
+++ functions.php
@@ -8,6 +8,10 @@
 function fz_enqueue_scripts()
 {
     wp_enqueue_style('custom-stylesheet', get_template_directory_uri() . '/style.css', array(), '1.0.0', 'all');
-    wp_enqueue_script('custom-script', get_template_directory_uri() . '/assests/main.js', array('jquery'), true);
+    wp_enqueue_style('bootstrap-style', get_template_directory_uri() . '/assets/src/library/css/bootstrap.min.css', array(), '1.0.0', 'all');
+
+    wp_enqueue_script('custom-script', get_template_directory_uri() . '/assets/main.js', array('jquery'), true);
+    wp_enqueue_script('bootstrap-script', get_template_directory_uri() . '/assets/src/library/js/bootstrap.min.js', array('jquery'), true);
+    wp_enqueue_script('bootstrap-bundle', get_template_directory_uri() . '/assets/src/library/js/bootstrap.bundle.min.js', array('jquery'), true);
 }
 add_action('wp_enqueue_scripts', 'fz_enqueue_scripts');
--- header.php
+++ header.php
@@ -11,9 +11,16 @@
     <head>
         <meta charset="<?php bloginfo('charset'); ?>">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ"
+            crossorigin="anonymous"></script>
         <?php wp_head(); ?>
     </head>
 
     <body <?php body_class(); ?>>
         <?php wp_body_open(); ?>
-        <header>Header</header>
+        <div id="post" class="site">
+            <header id="mastheader" class="site-header" role="banner">
+                <?php get_template_part('template-parts/header/nav'); ?>
+            </header>
+            <div id="content" class="site-content">
