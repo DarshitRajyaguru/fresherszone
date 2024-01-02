83d010f2b79f9410706d6b16812baf1352eaa095
DarshitRajyaguru <darshitrajyaguru@gmail.com>
2024-01-02 14:26:12 +0530

    bootstrap added

--- functions.php
+++ functions.php
@@ -8,5 +8,6 @@
 function fz_enqueue_scripts()
 {
     wp_enqueue_style('custom-stylesheet', get_template_directory_uri() . '/style.css', array(), '1.0.0', 'all');
+    wp_enqueue_script('custom-script', get_template_directory_uri() . '/assests/main.js', array('jquery'), true);
 }
 add_action('wp_enqueue_scripts', 'fz_enqueue_scripts');
