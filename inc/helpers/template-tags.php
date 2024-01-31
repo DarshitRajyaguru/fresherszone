567fec44acafb3d1dad1528d478bdcefa6628a6d
DarshitRajyaguru <darshitrajyaguru@gmail.com>
2024-01-31 18:57:41 +0530

    posts excerpt and read more added

--- inc/helpers/template-tags.php
+++ inc/helpers/template-tags.php
@@ -69,7 +69,7 @@ function fresherszone_posted_on() {
 		esc_attr( get_the_modified_date() )
 	);
 	$posted_on = sprintf(
-		esc_html_x( 'Posted on %s', 'post date', 'aquila' ),
+		esc_html_x( 'Posted on %s', 'post date', 'fresherszone' ),
 		'<a href="' . esc_url( $post_date_archive_permalink ) . '" rel="bookmark">' . $time_string . '</a>'
 	);
 	echo '<span class="posted-on text-secondary">' . $posted_on . '</span>';
@@ -82,9 +82,61 @@ function fresherszone_posted_on() {
  */
 function fresherszone_posted_by() {
 	$byline = sprintf(
-		esc_html_x( ' by %s', 'post author', 'aquila' ),
+		esc_html_x( ' by %s', 'post author', 'fresherszone' ),
 		'<span class="author vcard"><a href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
 	);
 
 	echo '<span class="byline text-secondary">' . $byline . '</span>';
+}
+
+/**
+ * Get the trimmed version of post excerpt.
+ *
+ * This is for modifing manually entered excerpts,
+ * NOT automatic ones WordPress will grab from the content.
+ *
+ * It will display the first given characters ( e.g. 100 ) characters of a manually entered excerpt,
+ * but instead of ending on the nth( e.g. 100th ) character,
+ * it will truncate after the closest word.
+ *
+ * @param int $trim_character_count Charter count to be trimmed
+ *
+ * @return bool|string
+ */
+function fresherszone_the_excerpt( $trim_character_count = 0 ) {
+	$post_ID = get_the_ID();
+
+	if ( empty( $post_ID ) ) {
+		return null;
+	}
+
+	if ( has_excerpt() || 0 === $trim_character_count ) {
+		the_excerpt();
+
+		return;
+	}
+
+	$excerpt = wp_html_excerpt( get_the_excerpt( $post_ID ), $trim_character_count, '[...]' );
+
+
+	echo $excerpt;
+}
+
+/**
+ * Filter the "read more" excerpt string link to the post.
+ *
+ * @param string $more "Read more" excerpt string.
+ *
+ * @return string (Maybe) modified "read more" excerpt string.
+ */
+function fresherszone_excerpt_more( $more = '' ) {
+
+	if ( ! is_single() ) {
+		$more = sprintf( '<a class="fresherszone-read-more text-white mt-3 btn btn-info" href="%1$s">%2$s</a>',
+			get_permalink( get_the_ID() ),
+			__( 'Read more', 'fresherszone' )
+		);
+	}
+
+	return $more;
 }
